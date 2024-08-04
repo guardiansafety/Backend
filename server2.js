bb66218a747d3c25ee5d470ab1c8c2ef1d4e9a76
Daniel Hong <danielh.toronto@gmail.com>
2024-08-04 02:36:43 -0400

    might be unstable again but added some sever2.js code

--- server2.js
+++ server2.js
@@ -14,9 +14,6 @@ const upload = multer({ dest: 'uploads/' });
 
 const uri = process.env.MONGODB_URI;
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Failed to connect to MongoDB', err));
 
 // Function to run the Python script
 function runPythonScript(photoPath, outputJsonPath, callback) {
